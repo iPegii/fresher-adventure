d3ebb4c7f6cc5fc305cc8bfe9ff4607c8ffa3ebb
iPegii <51372604+iPegii@users.noreply.github.com>
2022-01-16 16:29:14 +0200

    improve linting

--- docs/tuntikirjanpito.md
+++ docs/tuntikirjanpito.md
@@ -3,6 +3,7 @@
 
 Päivä   |Aika| Mitä tein
 --------|----|-----------
-11.1   |  2 | Sovelluksen suunnittelua
+11.1.   |  2 | Sovelluksen suunnittelua
+14.1.   |  2 | Ensimmäisen sivun tekemistä
 
 Yhteensä| 2 |
--- fresher_adventure_app/routes.py
+++ fresher_adventure_app/routes.py
@@ -1,27 +1,38 @@
-from flask import render_template, request, redirect
-from fresher_adventure_app import app
+from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from fresher_adventure_app import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///user"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/manage")
 def page1():
     return render_template("manage.html")
+
 
 @app.route("/checkpoint")
 def page2():
     return render_template("checkpoint.html")
 
+
 @app.route("/login", methods=["POST"])
 def login():
-    if request.method == 'POST': 
+    if request.method == 'POST':
         if request.form["submit"] == "signIn":
             username = request.form["username"]
             password = request.form["password"]
-            return render_template("result.html", username=username,
-                                          password=password)
+            # login and redirect to page
+            return render_template(
+                "index.html", username=username, password=password)
+        elif request.form["submit"] == "signUp":
+            username = request.form["username"]
+            password = request.form["password"]
+            return render_template(
+                "signup.html", username=username, password=password)

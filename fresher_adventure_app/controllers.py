f8dbc187c46a8f438e65b31fa8113f38838ce0ab
iPegii <hyper.vegeta@gmail.com>
2022-01-19 18:08:22 +0200

    fix signup

--- fresher_adventure_app/controllers.py
+++ fresher_adventure_app/controllers.py
@@ -7,9 +7,6 @@ from fresher_adventure_app.form import LoginForm, RegisterForm
 from fresher_adventure_app.db import User
 app.secret_key = getenv("SECRET_KEY")
 
-# Define the blueprint: 'auth', set its url prefix: app.url/auth
-mod_auth = Blueprint('auth', __name__, url_prefix='/auth')
-
 
 @app.route("/")
 def index():
@@ -27,14 +24,28 @@ def page2():
     return render_template("checkpoint.html")
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def sign_up():
-    form = RegisterForm()
-    return render_template(
-        "signup.html", form=form)
+    if request.method == "POST":
+        form = RegisterForm(request.form)
+        user = User.query.filter_by(
+            email=form.email.data, user=form.username.data).first()
+        if user is None:
+            hashed_password = generate_password_hash(form.password)
+            new_user = User(username=form.username, email=form.email,
+                            password=hashed_password, permission=0)
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/")
+        return flash('Error in sign up')
+
+    else:
+        form = RegisterForm()
+        return render_template(
+            "signup.html", form=form)
 
 
-@mod_auth.route("/login", methods=["POST"])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == 'POST':
         if request.form["submit_button"] == "signIn":
@@ -43,21 +54,14 @@ def login():
                 email=form.email.data).first()
             if user and check_password_hash(
                     user.password, form.password.data):
-                session['user_id'] = user.id
+                session['username'] = user.name
                 flash(f'Welcome {user.name}')
-    # login and redirect to page
-            return redirect("index.html")
+                return redirect("/")
         elif request.form["submit_button"] == "signUp":
-
-            """
             form = RegisterForm(request.form)
             return render_template(
-            "signup.html", username=form.username.data,
-            password=form.password.data, form=form)
-            """
-            form = RegisterForm()
-            username = request.args.get("username", None)
-            password = request.args.get("password", None)
-            return render_template(
-                "signup.html", username=username,
-                password=password, form=form)
+                "signup.html", username=form.username.data, form=form)
+        else:
+            return redirect("/")
+    else:
+        return redirect("/")

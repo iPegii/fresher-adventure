5c6e5b55870b10dad2ceda3d2c7e5e58c087d761
iPegii <51372604+iPegii@users.noreply.github.com>
2022-02-19 18:48:16 +0200

    fix schema typo

--- schema.sql
+++ schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE permission (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users NOT NULL,
     permission INTEGER DEFAULT 0 NOT NULL,
-    checkpoint_id INTEGER REFERENCES checkpoint, 
+    checkpoint_id INTEGER REFERENCES checkpoint,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
     modified_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp()
 );
@@ -41,7 +41,7 @@ CREATE TABLE location (
 CREATE TABLE point (
     id SERIAL PRIMARY KEY,
     point_amount INTEGER NOT NULL,
-    user_id INTEGER REFERENCES users NOT NULL
+    user_id INTEGER REFERENCES users NOT NULL,
     checkpoint_id INTEGER REFERENCES checkpoint NOT NULL,
     team_id INTEGER REFERENCES team NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp(),
